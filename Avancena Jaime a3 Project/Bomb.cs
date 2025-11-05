0d90a2babc163904108b4b61248485b5ea0c39db
JaimeA404 <jaimerafaavan@gmail.com>
2025-11-05 11:21:02 -0500

    drew the bomb, working on physics

---
+++ Avancena Jaime a3 Project/Bomb.cs
@@ -0,0 +1,44 @@
+﻿using MohawkGame2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+
+{
+    public class Bomb
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+        public Vector2 gravity;
+        Vector2 bombVelocity = new Vector2(0, 0);
+
+
+        public void Setup()
+        {
+            
+        }
+
+        public void Update() 
+        {
+            BombGravity();
+            DrawBomb();
+        }
+
+        public void BombGravity()
+        { 
+            bombVelocity += gravity * Time.DeltaTime;
+        }
+        public void DrawBomb()
+        {
+            Draw.LineSize = 2;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.DarkGray;
+            Draw.Circle(position, 7);
+
+        }
+    }
+}
---
+++ Avancena Jaime a3 Project/Game.cs
@@ -0,0 +1,48 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+        Bomb[] bombs = new Bomb[100];
+        int BombIndex = 0;
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Bombs Away");
+            Window.SetSize(800, 600);
+            Window.TargetFPS = 60;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.LightGray);
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                if (bombs[i] == null) continue;
+                bombs[i].Update();
+            }
+        }
+
+        void spawnBomb()
+        {
+            
+        }
+    }
+
+}
